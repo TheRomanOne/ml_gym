df738b06b138812f336b83c76a9a98beb7133709
TheRomanOne <roman.vlk18@Gmail.com>
2024-07-19 02:38:50 +0300

    started work

---
+++ Character.py
@@ -0,0 +1,109 @@
+import utils, random
+import numpy as np
+from panda3d.core import Vec3
+
+__MOBILITY__ = {
+        'SOLID': {
+            'linear': {'min': [0, 0, 0], 'max': [0, 0, 0]},
+            'angular': {'min': [0, 0, 0], 'max': [0, 0, 0]}
+            },
+
+        'LOOSE': {
+            'linear': {'min': [0, 0, -1], 'max': [0, 0, 1]},
+            'angular': {'min': [-0.2, -0.2, 0], 'max': [0.2, 0.2, 0]}
+            }
+}
+
+class Character:
+    def __init__(self, render, loader, world) -> None:
+        self.render = render
+        self.world = world
+        self.loader = loader
+
+        self.objects = []
+
+    def create_char_spikes(self, position, rotation=[0, 0, 0], scale=[1, 1, 1], color=[.78, .78, .78], static=False, mass=1):
+        player = self.get_box(position, rotation, scale, color, static, mass)
+        self.objects.append(player)
+        pos = 3 * [0]
+        for i in range(15):
+            v = np.random.uniform(1, 3, size=3)
+            if random.random() < .5: v[0] *= -1
+            if random.random() < .5: v[1] *= -1
+            if random.random() < .5: v[2] *= -1
+            v = v.tolist()
+            self.add_new_weight(player, position=v, scale=[.5, .5, .5], mass=.5, mobility=__MOBILITY__['SOLID'])
+            utils.get_line(v, pos).reparentTo(player)
+        return player
+    
+    def push_leg(self, char, leg_num, active):
+        char['affect']['movement']['active'] = active
+        if active:
+            leg = char['legs'][leg_num]
+            dist = char['character'].getPos(leg)
+            force = dist.normalized() * 100
+            char['affect']['movement']['force'] = force
+
+    def turn_leg(self, char, leg_num, active):
+        char['affect']['rotation']['active'] = active
+        if active:
+            force = [0, 0, 0]
+            if leg_num == 0:
+                force[0] = 100
+            elif leg_num == 1:
+                force[0] = -100
+            elif leg_num == 2:
+                force[2] = 100
+            elif leg_num == 3:
+                force[2] = -100
+
+            char['affect']['rotation']['force'] = force
+
+    def create_new(self, position, rotation=[0, 0, 0], scale=[1, 1, 1], color=[.78, .78, .78], static=False, mass=1):
+        player = self.get_box(position, rotation, scale, color, static, mass)
+        self.objects.append(player)
+        pos = 3 * [0]
+        legs = []
+        for i in [-2, 2]:
+            for j in [-2, 2]:
+                v = [i, j, -scale[0] - 2]
+                p2 = self.add_new_weight(player, position=v, scale=[scale[0]/2, scale[0]/2, scale[0]/7], mass=.5, mobility=__MOBILITY__['LOOSE'])
+                legs.append(p2)
+        
+        char = {
+            'character': player,
+            'legs': legs,
+            'affect': {
+                'movement': {'active': False, 'force': None},
+                'rotation': {'active': False, 'force': None}
+            }
+        }
+        
+        return char
+    
+    def get_box(self, position, rotation=[0, 0, 0], scale=[1, 1, 1], color=[.78, .78, .78], static=False, mass=1):
+        node = utils.new_box_node(scale, static, mass)
+        np = self.render.attachNewNode(node)
+        np.setHpr(*rotation)  
+        np.setPos(*position)
+
+        half_scale = [scale[0]/2, scale[1]/2, scale[2]/2]
+        model = self.loader.loadModel('models/box.egg')
+        model.setTextureOff(1)
+        model.setScale(*scale)
+        model.setColor(*color)
+        model.setPos(*[-h for h in half_scale])
+
+        model.reparentTo(np)
+        self.world.attachRigidBody(node)
+        return np
+
+    def add_new_weight(self, p1, position, scale=[1, 1, 1], mass=1, mobility=None):
+        position = Vec3(*position) + p1.getPos()
+        p2 = self.get_box(position=position, scale=scale, mass=mass)
+        constraint = utils.join(p1, p2, mobility)   
+        self.world.attachConstraint(constraint)
+        self.objects.append(p2)
+        return p2
+    
+    
---
+++ main.py
@@ -0,0 +1,182 @@
+from panda3d.core import LineSegs, TransformState, Vec4, Vec3, Point3, Geom, GeomNode, GeomVertexFormat, GeomVertexData, GeomTriangles, GeomVertexWriter, NodePath, Material, DirectionalLight, AmbientLight
+from panda3d.bullet import BulletGenericConstraint, BulletWorld
+from direct.showbase.ShowBase import ShowBase
+import random, utils
+import numpy as np
+from Character import Character
+
+
+
+class World(ShowBase):
+    def __init__(self) -> None:
+        ShowBase.__init__(self)
+        self.setBackgroundColor(0.4, .7, .8, 1)
+        self.camera = base.cam
+        self.camera.setPos(0, -70, 10)
+        self.camera.lookAt(0, 0, 0)
+        self.setFrameRateMeter(True)
+
+        # World
+        self.world = BulletWorld()
+        self.world.setGravity(Vec3(0, 0, -9.81))
+
+        taskMgr.add(self.update, 'update')
+
+        self.status = {
+            'rotation': [0, 0, 0],
+            'movement': [0, 0, 0],
+        }
+
+        self.Character = Character(render, loader, self.world)
+        self.player = None
+        self._leg_num = -1
+        self.add_plane()
+        self.init_models()
+        self.setup_keys()
+        self.add_lighting()
+    
+    def setup_keys(self):
+        f = 100
+        
+        self.accept('q', self.Character.push_leg, [self.player, 0, True])
+        self.accept('q-up', self.Character.push_leg, [self.player, 0, False])
+
+        self.accept('w', self.Character.push_leg, [self.player, 1, True])
+        self.accept('w-up', self.Character.push_leg, [self.player, 1, False])
+
+        self.accept('a', self.Character.push_leg, [self.player, 2, True])
+        self.accept('a-up', self.Character.push_leg, [self.player, 2, False])
+
+        self.accept('s', self.Character.push_leg, [self.player, 3, True])
+        self.accept('s-up', self.Character.push_leg, [self.player, 3, False])
+
+        self.accept('e', self.Character.turn_leg, [self.player, 0, True])
+        self.accept('e-up', self.Character.turn_leg, [self.player, 0, False])
+
+        self.accept('r', self.Character.turn_leg, [self.player, 1, True])
+        self.accept('r-up', self.Character.turn_leg, [self.player, 1, False])
+
+        self.accept('d', self.Character.turn_leg, [self.player, 2, True])
+        self.accept('d-up', self.Character.turn_leg, [self.player, 2, False])
+
+        self.accept('f', self.Character.turn_leg, [self.player, 3, True])
+        self.accept('f-up', self.Character.turn_leg, [self.player, 3, False])
+
+    def set_body_rotation(self, axis, value):
+        self.status['rotation'][axis] = value
+
+    def set_body_movement(self, axis, value):
+        self.status['movement'][axis] = value
+
+
+    def apply_drag(self, body):
+        fluid_density = .1225  # Density of air at sea level in kg/m^3
+
+        velocity = body.getLinearVelocity()
+        speed = velocity.length()
+        drag_force_magnitude = -fluid_density * speed**2
+        drag_force = velocity.normalized() * drag_force_magnitude
+        body.applyCentralForce(drag_force)
+
+
+    def rotate_body(self, body, angles):
+        current_hpr = body.getHpr()  # Get current HPR (heading, pitch, roll)
+        new_hpr = Point3(current_hpr)  # Create a new Point3 to modify
+        new_hpr.setX(new_hpr.getX() + angles[0])  # Increment X (heading) by 10 degrees
+        new_hpr.setY(new_hpr.getY() + angles[1])  # Increment X (heading) by 10 degrees
+        new_hpr.setZ(new_hpr.getZ() + angles[2])  # Increment X (heading) by 10 degrees
+        body.setHpr(new_hpr)  # Apply the new heading, pitch, and roll to p1
+
+
+    def init_models(self):
+        n = 1
+        m = 10
+        r = np.linspace(-n / 2, n / 2 + 1, n)
+        s = 3
+        for i in r:
+            for j in r:
+                c = self.Character.create_new(
+                    [i * m, j * m, 5], 
+                    [0, 0, 0],
+                    [s, s, s],
+                    [.2, .3, .8],
+                    False
+                )
+                if self.player is None:
+                    self.player = c
+
+            
+    def add_random_box(self, static=False):
+        p = [(random.random() - .5) * 5 for _ in range(3)]
+        r = [(random.random() - .5) * 180 for _ in range(3)]
+        s = [(.2 + .8 * random.random()) for _ in range(3)]
+        # s = 3 * [1 + .5 * random.random()]
+        # p[2] += 5
+        return self.add_box(p, r, s, [.8, .3, .2], False)
+
+    def add_box(self, position, rotation=[0, 0, 0], scale=[1, 1, 1], color=[.78, .78, .78], static=False, mass=1):
+        return self.Character.get_box(position, rotation, scale, color, static, mass)
+
+    def add_plane(self):
+        self.add_box(
+            position=[0, 0, -5],
+            scale=[100, 100, .1],
+            color=[.2, .8, .3],
+            static=True
+        )
+
+    def add_lighting(self):
+            # Create a directional light
+        directional_light = DirectionalLight("directional_light")
+        directional_light.setColor(Vec4(1, 1, 1, 1))
+        directional_light.setShadowCaster(True, 512, 512)
+        directional_light.getLens().setNearFar(1, 5500)
+        directional_light.getLens().setFilmSize(50, 50)
+
+        # Attach the light to the render node
+        directional_light_np = self.render.attachNewNode(directional_light)
+        directional_light_np.setHpr(-70, -60, 0)
+        self.render.setLight(directional_light_np)
+
+        # Create an ambient light
+        ambient_light = AmbientLight("ambient_light")
+        ambient_light.setColor(Vec4(0.8, 0.8, 0.8, 1))
+
+        # Attach the ambient light to the render node
+        ambient_light_np = self.render.attachNewNode(ambient_light)
+        self.render.setLight(ambient_light_np)
+
+        self.render.setShaderAuto()
+
+    # def create_material(self, color):
+    #     # Create a new material
+    #     mat = Material()
+        
+    #     # Set the diffuse color to red
+    #     mat.setDiffuse((*color, 1))
+        
+    #     # Optionally, set other material properties
+    #     # mat.setAmbient((0.2, 0, 0, 1))
+    #     mat.setSpecular((1, 1, 1, 1))
+    #     mat.setShininess(50.0)
+        
+    #     return mat
+    
+    def update(self, task):
+        dt = globalClock.getDt()
+        player = self.player['character']
+
+        # apply movement and rotation
+        for key, a in self.player['affect'].items():
+            if a['active']:
+                force = a['force']
+                utils.affect(key, player, force)
+              
+        # for o in self.Character.objects:
+        #     self.apply_drag(o.node())
+
+        self.world.doPhysics(dt)
+        return task.cont
+
+world = World()
+world.run()
